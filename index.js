37f2195b8c1a76bdaa79a57cb39936a50e8ff865
Will Brennan <willbrennan314@gmail.com>
2021-11-03 11:32:39 -0400

    commit tree 47c027cd124068809c61d6adb63a8740559da8d1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'it is not bob';
+}
+
+leastFavoriteCustomer;
